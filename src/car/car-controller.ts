19abfccbb9b66ff7ab3ac5da941944467c9241c2
Your Name <you@example.com>
2024-11-21 00:16:04 +0300

    build commit

--- src/car/car-controller.ts
+++ src/car/car-controller.ts
@@ -1,7 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CarCreateDto } from './dto/create-car-dto';
 import { CarService } from './car-service';
-import { Car } from 'database/models/car.model';
 import { ApiTags } from '@nestjs/swagger';
 
 
